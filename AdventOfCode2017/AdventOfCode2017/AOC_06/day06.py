08d04aa7334b019f6f5d51544320326fc1f6c69a
Jeroen <JeroenJacobs@Outlook.com>
2017-12-24 23:34:04 +0100

    Challenge 2017 day 06 A

---
+++ AdventOfCode2017/AdventOfCode2017/AOC_06/day06.py
@@ -0,0 +1,36 @@
+def redistributeMemory(memoryBanks):
+    # Get highest value
+    index = memoryBanks.index(max(memoryBanks))
+    # Empty memory bank and redistribute
+    blocks = memoryBanks[index]
+    memoryBanks[index] = 0;
+    for x in range(blocks):
+        # Check if index is within array
+        index += 1
+        if (index >= len(memoryBanks)):
+            index = 0
+
+        memoryBanks[index] += 1
+
+def findInfiniteLoop(input):
+    memoryBanks = [int(x) for x in input.split('\t')]
+    steps = 0
+    knownStates = []
+    while (memoryBanks not in knownStates):
+        
+        knownStates.append(list(memoryBanks))
+        redistributeMemory(memoryBanks)
+        steps += 1
+
+    return steps
+
+def day06_01(input):
+    return findInfiniteLoop(input)
+
+def day06_02(input):
+    return -1
+
+if __name__ == "__main__":
+    inputData = open('day06_input.txt').read()
+    print 'Day 06 01 The solution is', day06_01(inputData),'.'
+    print 'Day 06 02 The solution is', day06_02(inputData),'.'
--- AdventOfCode2017/AdventOfCode2017/AdventOfCode2017.py
+++ AdventOfCode2017/AdventOfCode2017/AdventOfCode2017.py
@@ -4,12 +4,14 @@ sys.path.insert(0, 'AOC_02')
 sys.path.insert(0, 'AOC_03')
 sys.path.insert(0, 'AOC_04')
 sys.path.insert(0, 'AOC_05')
+sys.path.insert(0, 'AOC_06')
 
 import day01
 import day02
 import day03
 import day04
 import day05
+import day06
 
 inputData = open('AOC_01/day01_input.txt').read()
 print 'Day 01 01 The solution to the captcha is', day01.day01_01(inputData),'.'
@@ -34,4 +36,9 @@ print '---'
 inputData = open('AOC_05/day05_input.txt').read()
 print 'Day 05 01 The number of steps to the exit is', day05.day05_01(inputData),'.'
 print 'Day 05 02 The number of steps to the exit is', day05.day05_02(inputData),'.'
+print '---'
+
+inputData = open('AOC_06/day06_input.txt').read()
+print 'Day 06 01 The total number of cycles to an infinite loop is', day06.day06_01(inputData),'.'
+print 'Day 06 02 The number of cycles in the infinite loop is', day06.day06_02(inputData),'.'
 print '---'
