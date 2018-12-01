5d9dff5843baa49e19e91609a48cce04ae3eef33
Jeroen <JeroenJacobs@Outlook.com>
2018-12-01 12:06:50 +0100

    AOC 2018 Day 01

---
+++ AdventOfCode2018/Aoc01/Program.cs
@@ -0,0 +1,50 @@
+﻿using AocLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (Input.Read(args, out string[] input))
+      {
+        Console.WriteLine($"Assignment 1: [{Assignment1(input)}].");
+        Console.WriteLine($"Assignment 2: [{Assignment2(input)}].");
+      }
+      else
+      {
+        Console.WriteLine($"Failed to open file.");
+      }
+      Console.ReadLine();
+    }
+
+    private static int Assignment1(string[] input)
+    {
+      int freq = 0;
+      input.ToList().ForEach(f => freq += int.Parse(f));
+      return freq;
+    }
+
+    private static int Assignment2(string[] input)
+    {
+      int freq = 0;
+      var freqs = new List<int>() { freq };
+
+      while (true)
+      {
+        foreach (var line in input)
+        {
+          freq += int.Parse(line);
+          if (freqs.Contains(freq))
+          {
+            return freq;
+          }
+          freqs.Add(freq);
+        }
+      }
+    }
+  }
+}
---
+++ AdventOfCode2018/AocLib/Input.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace AocLib
+{
+  public static class Input
+  {
+    /// <summary>
+    /// Default input file.
+    /// </summary>
+    private static readonly string DefaultFile = "input.txt";
+
+    /// <summary>
+    /// Read a file from the disk.
+    /// </summary>
+    /// <param name="filePath">Filepath of the file to read.</param>
+    /// <param name="content">Returned content of the file.</param>
+    public static bool Read(string filePath, out string[] content)
+    {
+      if (File.Exists(filePath))
+      {
+        content = File.ReadAllLines(filePath);
+        return true;
+      }
+
+      content = new string[0];
+      return false;
+    }
+
+    /// <summary>
+    /// Read content of input file from arguments.
+    /// If not available reads the default file <see cref="DefaultFile"/>.
+    /// </summary>
+    /// <param name="args">Command line Arguments</param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public static bool Read(string[] args, out string[] content)
+    {
+      string filePath = (args.Length < 1) ? DefaultFile : args[0];
+      return Read(filePath, out content);
+    }
+  }
+}
