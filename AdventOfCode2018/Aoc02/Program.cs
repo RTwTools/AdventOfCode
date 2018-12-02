7fd74299da73510b5d1486d2387396ea69c41ce9
Jeroen <JeroenJacobs@Outlook.com>
2018-12-02 10:58:02 +0100

    AOC 2018 Day 02

---
+++ AdventOfCode2018/Aoc02/Program.cs
@@ -0,0 +1,59 @@
+﻿using AocLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (Input.Read(args, out string[] input))
+      {
+        Console.WriteLine($"Assignment 1: [{Assignment1(input, out List<string> boxIds)}].");
+        Console.WriteLine($"Assignment 2: [{Assignment2(boxIds)}].");
+      }
+      else
+      {
+        Console.WriteLine($"Failed to open file.");
+      }
+      Console.ReadKey();
+    }
+
+    private static int Assignment1(string[] input, out List<string> boxIds)
+    {
+      var words = new List<Dictionary<char, int>>();
+      var ids = new HashSet<string>();
+
+      input.ToList().ForEach(boxId => 
+      {
+        words.Add(StringUtility.CountChars(boxId));
+        if (words.Last().Values.Max() > 1) ids.Add(boxId);
+      });
+      boxIds = ids.ToList();
+
+      var twoCount = words.Where(w => w.ContainsValue(2)).Count();
+      var threeCount = words.Where(w => w.ContainsValue(3)).Count();
+      
+      return twoCount * threeCount;
+    }
+
+    private static string Assignment2(List<string> boxIds)
+    {
+      var commonLetters = new List<char>();
+
+      boxIds.ForEach(id1 =>
+      {
+        boxIds.ForEach(id2 =>
+        {
+          if (id1 == id2) return;
+          var letters = StringUtility.EqualChars(id1, id2);
+          if (letters.Count > commonLetters.Count) commonLetters = letters;
+        });
+      });
+
+      return new string(commonLetters.ToArray());
+    }
+  }
+}
---
+++ AdventOfCode2018/AocLib/StringUtility.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AocLib
+{
+  public static class StringUtility
+  {
+    /// <summary>
+    /// Counts how often every char occurs in a word. 
+    /// </summary>
+    /// <param name="word">Word.</param>
+    public static Dictionary<char, int> CountChars(string word)
+    {
+      var letters = new Dictionary<char, int>();
+
+      foreach (var letter in word)
+      {
+        if (!letters.ContainsKey(letter))
+        {
+          letters.Add(letter, 1);
+        }
+        else
+        {
+          letters[letter] += 1;
+        }
+      }
+      return letters;
+    }
+
+    /// <summary>
+    /// Creates a list of all the chars that are equal at the same location in the string.
+    /// </summary>
+    /// <param name="word1">Word 1.</param>
+    /// <param name="word2">Word 2.</param>
+    public static List<char> EqualChars(string word1, string word2)
+    {
+      return word1.Where((@char, index) => @char == word2[index]).ToList();
+    }
+  }
+}
