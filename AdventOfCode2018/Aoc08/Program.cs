cada75b71829f8417bf338e9f6e9b6689de0465b
Jeroen <JeroenJacobs@Outlook.com>
2018-12-08 14:55:00 +0100

    AOC 2018 Day 08

---
+++ AdventOfCode2018/Aoc08/Program.cs
@@ -0,0 +1,67 @@
+﻿using AocLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc08
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (Input.Read(args, out string[] input))
+      {
+        var node = Node.Parse(input[0].Split(' ').Select(int.Parse).ToList());
+        Console.WriteLine($"Assignment 1: [{node.MetaDataSum}].");
+        Console.WriteLine($"Assignment 2: [{node.Value}].");
+      }
+      else
+      {
+        Console.WriteLine($"Failed to open file.");
+      }
+      Console.ReadKey();
+    }
+  }
+
+  class Node
+  {
+    private static readonly int HeaderSize = 2;
+    public List<Node> Childeren { get; set; }
+    public List<int> Metadata { get; set; }
+    public int SizeOf { get { return SizeOfChilderen + HeaderSize + Metadata.Count; } }
+    public int SizeOfChilderen { get { return Childeren.Sum(c => c.SizeOf); } }
+    public int MetaDataSum { get { return Childeren.Sum(c => c.MetaDataSum) + Metadata.Sum(); } }
+
+    public int Value
+    {
+      get
+      {
+        return (Childeren.Any())
+          ? Metadata.Where(m => m <= Childeren.Count).Sum(m => Childeren[m - 1].Value)
+          : MetaDataSum;
+      }
+    }
+
+    public Node()
+    {
+      Childeren = new List<Node>();
+      Metadata = new List<int>();
+    }
+
+    public static Node Parse(List<int> values)
+    {
+      int childerenCount = values[0];
+      int metadataCount = values[1];
+      var node = new Node();
+
+      for (int i = 0; i < childerenCount; i++)
+      {
+        node.Childeren.Add(Parse(values.Skip(HeaderSize + node.SizeOfChilderen).ToList()));
+      }
+
+      node.Metadata.AddRange(values.GetRange(node.SizeOfChilderen + HeaderSize, metadataCount));
+
+      return node;
+    }
+  }
+}
