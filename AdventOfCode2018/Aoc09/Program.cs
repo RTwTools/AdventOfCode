47c3d01d923d81fa0093caa63330068b1919afab
Jeroen <JeroenJacobs@Outlook.com>
2018-12-09 15:31:14 +0100

    AOC 2018 Day 09

---
+++ AdventOfCode2018/Aoc09/Program.cs
@@ -0,0 +1,52 @@
+﻿using AocLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc09
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"Assignment 1: [{new MarbleGame(471, 72026).Players.Max()}].");
+      Console.WriteLine($"Assignment 2: [{new MarbleGame(471, 7202600).Players.Max()}].");
+      Console.ReadKey();
+    }
+
+    class MarbleGame
+    {
+      public LinkedList<long> Marbles = new LinkedList<long>();
+      public List<long> Players { get; set; }
+      public LinkedListNode<long> Current { get; set; }
+
+      public MarbleGame(int players, int marbles)
+      {
+        Current = Marbles.AddFirst(0);
+        Players = new List<long>(new long[players]);
+
+        for (int i = 0; i < marbles; i++)
+        {
+          Players[i % Players.Count] += AddMarble(i + 1);
+        }
+      }
+
+      private long AddMarble(long value)
+      {
+        if (value % 23 == 0)
+        {
+          Current = Current.PreviousOrLast(6);
+          long score = value + Current.PreviousOrLast().Value;
+          Marbles.Remove(Current.PreviousOrLast());
+
+          return score;
+        }
+        else
+        {
+          Current = Marbles.AddAfter(Current.NextOrFirst(), value);
+          return 0;
+        }
+      }
+    }
+  }
+}
---
+++ AdventOfCode2018/AocLib/CircularLinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace AocLib
+{
+  public static class CircularLinkedList
+  {
+    public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
+    {
+      return current.Previous ?? current.List.Last;
+    }
+
+    public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
+    {
+      return current.Next ?? current.List.First;
+    }
+
+    public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current, int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+        current = current.PreviousOrLast();
+      }
+      return current;
+    }
+
+    public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current, int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+        current = current.NextOrFirst();
+      }
+      return current;
+    }
+  }
+}
